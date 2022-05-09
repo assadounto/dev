a6d4cdd9de73a8f75781908f41e1043452c1e098
RICH <adukyerer@gmail.com>
2022-05-09 20:04:27 +0000

    fix pop up

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function openNav() {
-  document.getElementById('Mynav').style.display = 'block';
-  document.getElementById('head').style.filter = 'blur(0.4rem)';
-  document.getElementById('mob-nav').style.display = 'none';
+  document.getElementById("Mynav").style.display = "block";
+  document.getElementById('head').style.filter = 'blur(0.3rem)';
+  document.getElementById('logo').style.filter = 'blur(0.3rem)';
+  document.getElementById('hamb').style.display = 'none';
+}
+function closeNav() {
+  document.getElementById("Mynav").style.display = "none";
+  document.getElementById('head').style.filter = 'none';
+  document.getElementById('hamb').style.display = 'block';
 }
