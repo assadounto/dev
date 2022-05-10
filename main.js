aa87dea19efcba45fbc04b528d6ef03c2ba9a27f
RICH <adukyerer@gmail.com>
2022-05-10 08:47:44 +0000

    fix linter error

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 let isClicked = false;
 
-  function openNav() {
+function openNav() {
   if (!isClicked) {
-    document.getElementById("Mynav").style.display = 'block';
+    document.getElementById('Mynav').style.display = 'block';
     document.getElementById('head').style.filter = 'blur(0.3rem)';
     document.getElementById('logo').style.filter = 'blur(0.3rem)';
     document.getElementById('hamb').style.display = 'none';
     isClicked = true;
-  }
-    else {
-    document.getElementById("Mynav").style.display = 'none';
+  } else {
+    document.getElementById('Mynav').style.display = 'none';
     document.getElementById('head').style.filter = 'none';
     document.getElementById('hamb').style.display = 'block';
     document.getElementById('logo').style.filter = 'none';
     isClicked = false;
- }
+  }
 }
- function menuClicked() {
+
+// eslint-disable-next-line no-unused-vars
+function menuClicked() {
   openNav();
 }
