7c36840d7715cedfdd2ba9f8c42389798c869d38
RICH <adukyerer@gmail.com>
2022-05-10 08:00:06 +0000

    fix linters

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+let isClicked = false;
 function openNav() {
+  if (!isClicked) {
   document.getElementById("Mynav").style.display = "block";
   document.getElementById('head').style.filter = 'blur(0.3rem)';
   document.getElementById('logo').style.filter = 'blur(0.3rem)';
   document.getElementById('hamb').style.display = 'none';
-  openNav();
-}
-
-function closeNav() {
+  isClicked = true;
+  }
+ else {
   document.getElementById("Mynav").style.display = "none";
   document.getElementById('head').style.filter = 'none';
   document.getElementById('hamb').style.display = 'block';
   document.getElementById('logo').style.filter = 'none';
-  closeNav();
+  isClicked = false;
+ }
+}
+function menuClicked() {
+  openNav();
 }
