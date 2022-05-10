576c7b53891ca2b434919084efe7fad0bc4b596a
RICH <adukyerer@gmail.com>
2022-05-10 08:05:38 +0000

    fix indentation

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 let isClicked = false;
-function openNav() {
+
+  function openNav() {
   if (!isClicked) {
-  document.getElementById("Mynav").style.display = "block";
-  document.getElementById('head').style.filter = 'blur(0.3rem)';
-  document.getElementById('logo').style.filter = 'blur(0.3rem)';
-  document.getElementById('hamb').style.display = 'none';
-  isClicked = true;
+    document.getElementById("Mynav").style.display = 'block';
+    document.getElementById('head').style.filter = 'blur(0.3rem)';
+    document.getElementById('logo').style.filter = 'blur(0.3rem)';
+    document.getElementById('hamb').style.display = 'none';
+    isClicked = true;
   }
- else {
-  document.getElementById("Mynav").style.display = "none";
-  document.getElementById('head').style.filter = 'none';
-  document.getElementById('hamb').style.display = 'block';
-  document.getElementById('logo').style.filter = 'none';
-  isClicked = false;
+    else {
+    document.getElementById("Mynav").style.display = 'none';
+    document.getElementById('head').style.filter = 'none';
+    document.getElementById('hamb').style.display = 'block';
+    document.getElementById('logo').style.filter = 'none';
+    isClicked = false;
  }
 }
-function menuClicked() {
+ function menuClicked() {
   openNav();
 }
