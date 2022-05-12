5738a53d0482b0346bdcf972f9d00ed75824a561
RICH <adukyerer@gmail.com>
2022-05-12 01:38:19 +0000

    add objects dynamics

--- main.js
+++ main.js
@@ -24,10 +24,13 @@ function menuClicked() {
 const projects = [
   {
     id: 0,
+    place: 'CANOPY',
+    title: 'Back End Dev',
+    year: '2015',
     name: 'Tonic',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    technologies: ['Ruby on rails', 'css', 'javascript', 'html'],
+    technologies: ['html', 'css', 'javascript', 'Ruby', 'github', 'Boostrap'],
     image: {
       link: 'logos/tonic-project-desktop.svg',
       alt: 'Project Screenshot',
@@ -39,8 +42,11 @@ const projects = [
   {
     id: 1,
     name: 'Multi-Post Stories',
+    place: 'Facebook',
+    title: 'Full Stack Dev',
+    year: '2015',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     technologies: ['Ruby on rails', 'css', 'javascript', 'html'],
     image: {
       link: 'logos/multi-post-stories-project.svg',
@@ -52,11 +58,14 @@ const projects = [
   {
     id: 2,
     name: 'Facebook 360',
+    place: 'Facebook',
+    title: 'Full Stack Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     technologies: ['Ruby on rails', 'css', 'javascript', 'html'],
     image: {
-      link: 'logos/popup.png',
+      link: 'logos/facebook-360-project.svg',
       alt: 'Facebook 360screenshot',
     },
     liveSource: '#',
@@ -65,24 +74,14 @@ const projects = [
   {
     id: 3,
     name: 'Uber',
+    place: 'Uber',
+    title: 'Lead Developer',
+    year: '2018',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     technologies: ['Ruby on rails', 'css', 'javascript', 'html'],
     image: {
-      link: 'logos/popup.png',
-      alt: 'Uber Navigation screenshot',
-    },
-    liveSource: '#',
-    sourceCode: '#',
-  },
-  {
-    id: 4,
-    name: 'Uber',
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    technologies: ['Ruby on rails', 'css', 'javascript', 'html'],
-    image: {
-      link: 'logos/popup.png',
+      link: 'logos/uber-navigation-project.svg',
       alt: 'Uber Navigation screenshot',
     },
     liveSource: '#',
@@ -99,45 +98,46 @@ function showDetails(projectIndex) {
   <div class="details-container">
     <div class="details-container-content">
     <div class="headc">
-    <div class= "headtitle">${projects[projectIndex].name}</div>
+    <h1 class= "headtitle">${projects[projectIndex].name}</h1>
+    <div class="works__card__subtitle flex">
+              <p class="works__card__subtitle-1 c1">
+              <b>${projects[projectIndex].place}</b>
+              </p>
+              <ul class="works__card__subtitle-ul flex">
+                <li class="works__card__subtitle-li c2">
+                ${projects[projectIndex].title}
+                </li>
+                <li class="works__card__subtitle-li c2"> 
+                ${projects[projectIndex].year}
+                </li>
+              </ul>
+            </div>
     <div class="closeDetails" id="closeDetails">&times;</div>
     </div>
-     
-      <img class="project-image" src="${
-        projects[projectIndex].image.link
-      }" alt="${projects[projectIndex].image.alt}" />
-      <div class="project-info">
-        </div>
-      <div class="tech-and-buttons">
-        <div>
-        <p class="description">
-         ${projects[projectIndex].description}
-        </p>
-        </div>
-        <div>
-        <div>  
-          <ul class="card-languages">
+      <img class="project-image" src="${projects[projectIndex].image.link}" alt="${projects[projectIndex].image.alt}" />
+       <div class="s1"> 
+      <p class="description">
+        ${projects[projectIndex].description}
+       </p>
+
+        <div class="tech-and-buttons">
+          <div class="card-languages">
             ${(function usedTech() {
-              return projects[projectIndex].technologies
-                .map((tech) => `<li class="language">${tech}</li>`)
-                .join('');
-            })()}
-          </ul>
-        </div>
-        <div class="">
-          <a  href="${
-            projects[projectIndex].liveSource
-          }" class="btn green-button b1">See Live <i class="fa fa-external-link"></i></a>
-           <a href="${
-             projects[projectIndex].sourceCode
-           }" class="btn green-button">See Source <i class="fa fa-github"></i></a>
-        </div>
-
-        </div>
+    return projects[projectIndex].technologies
+      .map((tech) => `<li class="language">${tech}</li>`)
+      .join('');
+  }())}
           </div>
-
-          
-      
+          <div class="buttons">
+            <a href="${
+  projects[projectIndex].liveSource
+}" class="btn green-button">See Live <img src="logos/Icon.png"></i></a>
+             <a href="${
+  projects[projectIndex].sourceCode
+}" class="btn green-button">See Source <img src="logos/Vector.png"></i></a>
+          </div>
+          </div>
+    </div>
   </div>`;
 
   modalDetails.innerHTML += projectsCode;
@@ -145,6 +145,7 @@ function showDetails(projectIndex) {
   document.getElementById('closeDetails').addEventListener('click', () => {
     modalDetails.innerHTML = '';
     document.body.removeChild(modalDetails);
+    document.getElementById('hero').style.filter = 'none';
   });
 }
 
@@ -163,10 +164,10 @@ for (let j = 0; j < projects.length; j += 1) {
             <div class="works__card__text">
               <h2 class="works__card__title">${projects[j].name}</h2>
               <div class="works__card__subtitle flex">
-                <p class="works__card__subtitle-1">CANOPY</p>
+                <p class="works__card__subtitle-1">${projects[j].place}</p>
                 <ul class="works__card__subtitle-ul flex">
-                  <li class="works__card__subtitle-li">Back End Dev</li>
-                  <li class="works__card__subtitle-li">2015</li>
+                  <li class="works__card__subtitle-li">${projects[j].title}</li>
+                  <li class="works__card__subtitle-li">${projects[j].year}</li>
                 </ul>
               </div>
               <p class="works__card__dsc p1">
@@ -179,7 +180,9 @@ for (let j = 0; j < projects.length; j += 1) {
                 <p class="works__card__topics__text p2 w1">javascript</p>
               </div>
               <div class="works__card__btn">
-                <button type="button" class="btn b1 details-btn " type="button" data-id="${projects[j].id}">See Project</button>
+                <button type="button" class="btn b1 details-btn " type="button" data-id="${
+  projects[j].id
+}">See Project</button>
               </div>
             </div>
           </li>`;
@@ -190,5 +193,6 @@ const showButton = document.querySelectorAll('.details-btn');
 showButton.forEach((btn) => {
   btn.addEventListener('click', (event) => {
     showDetails(event.target.dataset.id);
+    document.getElementById('hero').style.filter = 'blur(1.9rem)';
   });
 });
